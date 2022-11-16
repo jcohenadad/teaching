eea2f57145fbc44980b6fc74a98e44a00b69d356
jcohenadad <jcohen@polymtl.ca>
2022-11-16 14:13:36 -0500

    Loop across forms, retrieve content

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Collection of scripts to help organize teaching activities (create grading forms
 Some scripts require Google OAuth2.0 authentication. As a prerequisite, do this:
 https://pythonhosted.org/PyDrive/quickstart.html#authentication
  
+# Prerequisite (Google API)
+
+- Enable [Google Forms API](https://console.developers.google.com/apis/api/forms.googleapis.com/overview?project=1036953068115) 
--- gbm6125_grade_oral.py
+++ gbm6125_grade_oral.py
@@ -10,17 +10,38 @@
 #
 # Author: Julien Cohen-Adad
 
-# import requests
-from pydrive.auth import GoogleAuth
-
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
-
-
-# Parameters to setup
+# Parameters
 folder_gform = "1DjaqpTOp1QAy042qlFBoCpTY3wDA8lZp"  # Folder that contains all Google Forms
 
-gsheet = requests.get(f'{PUBLICATIONS}/export?format=xlsx')
+# import requests
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import client, file, tools
 
-# TODO: input
+# Pydrive auth
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
 
+# Google API auth
+SCOPES = "https://www.googleapis.com/auth/forms.body.readonly"
+DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
+store = file.Storage('token.json')
+creds = None
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = discovery.build('forms', 'v1', http=creds.authorize(
+    Http()), discoveryServiceUrl=DISCOVERY_DOC, static_discovery=False)
+
+
+# Auto-iterate through all files that matches this query
+drive = GoogleDrive(gauth)
+file_list = drive.ListFile({'q': "'{}' in parents".format(folder_gform)}).GetList()
+for file1 in file_list:
+    form_id = file1['id']
+    print('title: %s, id: %s' % (file1['title'], form_id))
+
+    result = service.forms().get(formId=form_id).execute()
+    print(result)
