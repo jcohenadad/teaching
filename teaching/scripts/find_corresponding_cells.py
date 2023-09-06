974915f85c21dd9f4adcf0e1eb4c7b4dad0f68a1
jcohenadad <jcohen@polymtl.ca>
2023-09-05 23:01:53 -0400

    Now showing cells without match

--- teaching/scripts/find_corresponding_cells.py
+++ teaching/scripts/find_corresponding_cells.py
@@ -12,46 +12,48 @@ def read_csv(filename, column_number, delimiter):
                 data.append(row[column_number])
     return data
 
-def find_correspondence(file1, column1, delimiter1, file2, column2, delimiter2):
+def find_non_correspondence(file1, column1, delimiter1, file2, column2, delimiter2):
     """
-    Find the correspondance between two CSV files based on the specified columns.
+    Find lines that do not correspond between two CSV files based on the specified columns.
     """
     data1 = read_csv(file1, column1, delimiter1)
     data2 = read_csv(file2, column2, delimiter2)
 
-    correspondence = []
+    non_correspondence1 = []
+    non_correspondence2 = []
 
     for i, val1 in enumerate(data1):
-        for j, val2 in enumerate(data2):
-            if val1 == val2:
-                correspondence.append((i, j))
+        if val1 not in data2:
+            non_correspondence1.append(i)
 
-    return correspondence
+    for j, val2 in enumerate(data2):
+        if val2 not in data1:
+            non_correspondence2.append(j)
+
+    return non_correspondence1, non_correspondence2
 
 def main():
     # Take inputs from the user
-    # file1 = input("Enter the name of the first CSV file: ")
-    # column1 = int(input(f"Enter the column number to match in {file1}: "))
-    # file2 = input("Enter the name of the second CSV file: ")
-    # column2 = int(input(f"Enter the column number to match in {file2}: "))
-    file1 = 'src1.CSV'
-    column1 = 0
-    delimiter1 = ';'
-    file2 = 'dest.csv'
-    column2 = 1
-    delimiter2 = ','
+    file1 = 'src1.CSV'  #input("Enter the name of the first CSV file: ")
+    column1 = 0  #int(input(f"Enter the column number to match in {file1}: "))
+    delimiter1 = ';'  #input(f"Enter the delimiter for {file1} (e.g., ',' or ';'): ")
+    
+    file2 = 'dest.csv'  #input("Enter the name of the second CSV file: ")
+    column2 = 1  #int(input(f"Enter the column number to match in {file2}: "))
+    delimiter2 = ','  #input(f"Enter the delimiter for {file2} (e.g., ',' or ';'): ")
 
-
-    # Find the correspondance
-    correspondence = find_correspondence(file1, column1, delimiter1, file2, column2, delimiter2)
+    # Find non-corresponding lines
+    non_correspondence1, non_correspondence2 = find_non_correspondence(file1, column1, delimiter1, file2, column2, delimiter2)
 
     # Print the results
-    if correspondence:
-        print("Corresponding lines between the two files:")
-        for i, j in correspondence:
-            print(f"Line {i+1} in {file1} corresponds to line {j+1} in {file2}.")
+    if non_correspondence1 or non_correspondence2:
+        print("Lines that do not have corresponding matches:")
+        for i in non_correspondence1:
+            print(f"Line {i+1} in {file1} does not have a corresponding match in {file2}.")
+        for j in non_correspondence2:
+            print(f"Line {j+1} in {file2} does not have a corresponding match in {file1}.")
     else:
-        print("No correspondence found between the two files.")
+        print("All lines have corresponding matches between the two files.")
 
 if __name__ == '__main__':
     main()
