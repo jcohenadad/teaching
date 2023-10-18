1f20af39e5f2798812f230093d2c2a1afce8bdc1
jcohenadad <jcohen@polymtl.ca>
2023-10-18 11:11:53 -0400

    Order column based on the questions

--- teaching/utils/utils.py
+++ teaching/utils/utils.py
@@ -39,34 +39,10 @@ def fetch_responses(results, result_metadata):
             responses_dict[question] = result_item
         responses_list.append(responses_dict)
 
-
-        #     result_item = j['textAnswers']['answers'][0]['value']
-        #     # Hack: because results are not sorted (ie: matricule is not the first item), we need to look for the
-        #     # matricule based on its properties: digit and length of 7.
-        #     if result_item == '000000':
-        #         # that's me :)
-        #         matricule = result_item
-        #         is_prof = True
-        #     elif len(result_item) == 7 and result_item.isdigit():
-        #         matricule = result_item
-        #     elif result_item.isdigit():
-        #         value.append(result_item)
-        #     else:
-        #         logger.debug(f"Comment: {result_item}")
-        # # Make sure a matricule was found
-        # if matricule == '':
-        #     raise RuntimeError('Problem identifying matricule.')
-        
-        # grade = np.sum([int(i) for i in value])
-        # logger.debug('Matricule: {} | Grade: {}'.format(matricule, grade))
-        # if is_prof:
-        #     # Prof response
-        #     gradeProf = grade
-        # else:
-        #     # Student response
-        #     gradeStudent.append(grade)
-
     df = pd.DataFrame(responses_list)
-    # df = pd.DataFrame(list(responses_dict.items()), columns=['Question', 'Response'])
-    df.set_index('Question', inplace=True)
+
+    # Order columns based on the question_lookup order
+    ordered_columns = [question_lookup[question_id] for question_id in question_lookup]
+    df = df[ordered_columns]
+    
     return df
