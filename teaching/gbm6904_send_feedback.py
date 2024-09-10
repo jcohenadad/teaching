809d001b19be536b5d73be384d7641180c26a971
jcohenadad <jcohen@polymtl.ca>
2024-09-09 22:10:39 -0400

    Updated to 2024 links

--- teaching/gbm6904_send_feedback.py
+++ teaching/gbm6904_send_feedback.py
@@ -3,6 +3,7 @@
 # For course GBM6904. Fetch Google Form (providing ID of the form), gather and email feedback to the student.
 #
 # How to use:
+# - Make sure the Parameters (below) are set correctly
 # - Open the gsheet with the list of presentations
 # - Copy the matricule of the first presenting student
 # - Run this function: 
@@ -35,14 +36,14 @@ from .utils.utils import fetch_responses, expand_url, gmail_send_message, fetch_
 
 
 # Parameters
-FOLDER_ID = '1rj6GfMvK6_cirTHPYpExSPJtZHne-gM3'  # ID of the folder that includes all the gforms
-SPREADSHEET_ID = '11vpuK2iiuIpUscjfI-Ork9fg0BzU3OFzuG9_-aweEDY'  # Google sheet that lists the matricules and URLs to the gforms
+FOLDER_ID = '1I7S-RHJ9WgQAGa51RuVmRhe2sddHPrqa'  # ID of the Google folder that includes all the gforms (go to the folder, click on "Open in new window", and copy the ID from the URL)
+SPREADSHEET_ID = '1fOREGH2EFc1gxTDMrMwmhjX-QBk20BvqncugHvy85lE'  # Google sheet that lists the matricules and URLs to the gforms
+PATH_CSV = "/Users/julien/Library/CloudStorage/Dropbox/documents/cours/GBM6904_seminaires/2024/GBM6904-20243-01C.CSV"  # Path to the CSV file that links matricule to email address
 MATRICULE_ID = 0  # ID of the question corresponding to the matricule
 MATRICULE_JULIEN = '000000'
 FEEDBACK_ID = 11  # ID of the question corresponding to the feedback
 # TODO: have the address below in local config files
 EMAIL_FROM = "jcohen@polymtl.ca"
-PATH_CSV = "/Users/julien/Dropbox/documents/cours/GBM6904_seminaires/2023/GBM6904-7904-20233-01C.csv"
 LOGGING_LEVEL = 'INFO'  # 'DEBUG', 'INFO'
 
 # Initialize colored logging
